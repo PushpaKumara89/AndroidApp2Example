853d066948845f1f78a52e0c39e9f8d88591c26d
kpushpakumara89 <kpushpakumarajd@gmail.com>
2022-11-30 21:42:10 +0530

    List View - Array Adapter (data source- Array , Array Adapter)

--- app/src/main/java/com/pkp/toast/MainActivity.java
+++ app/src/main/java/com/pkp/toast/MainActivity.java
@@ -2,73 +2,75 @@ package com.pkp.toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 public class MainActivity extends AppCompatActivity {
-    //import necessary class
-    Button btnSave,btnGetData;
-    EditText txtName,txtAge;
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //get references
-        btnSave = findViewById(R.id.btnSaveData);
-        btnGetData = findViewById(R.id.btnGetData);
+        // Array
+        String[] countries = new String[]{"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
+                "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba",
+                "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados",
+                "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina",
+                "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam",
+                "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde",
+                "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island",
+                "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the",
+                "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus",
+                "Czech Republic","Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador",
+                "Egypt", "El Salvador","Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia",
+                "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan",
+                "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia",
+                "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala",
+                "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands",
+                "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia",
+                "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan",
+                "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of",
+                "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho",
+                "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau",
+                "Macedonia", "The Former Yugoslav Republic of",
+                "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique",
+                "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of",
+                "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar",
+                "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
+                "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway",
+                "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines",
+                "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation",
+                "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa",
+                "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone",
+                "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa",
+                "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena",
+                "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland",
+                "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan",
+                "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago",
+                "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine",
+                "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands",
+                "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)",
+                "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia",
+                "Zambia", "Zimbabwe", "Palestine"};
 
-        txtName = findViewById(R.id.name);
-        txtAge = findViewById(R.id.age);
+        ListAdapter listAdapter = new ArrayAdapter<String>(this, R.layout.list_item,countries);
+        listView = findViewById(R.id.list_view);
+        listView.setAdapter(listAdapter);
 
-        //save button action
-        btnSave.setOnClickListener(new View.OnClickListener() {
+        //item view Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                String name = txtName.getText().toString();
-                int age = 0;
-                try {Thread.sleep(1000);}catch (Exception e){}
-                try {
-                    age = Integer.parseInt(txtAge.getText().toString());
-                }catch (NumberFormatException n){
-                    FancyToast.makeText(getApplicationContext(),"age allow only number",FancyToast.LENGTH_LONG,FancyToast.WARNING,true).show();
-                    return;
-                }
-
-                if (name.isEmpty() | age < 0 | age > 80){
-                    FancyToast.makeText(getApplicationContext(),"check your Data",FancyToast.LENGTH_LONG,FancyToast.WARNING,true).show();
-                    return;
-                }
-
-                SharedPreferences prf = getSharedPreferences("details", MODE_PRIVATE);
-                SharedPreferences.Editor editor = prf.edit();
-                editor.putString("name", name);
-                editor.putInt("age", age);
-                boolean commit = editor.commit();
-                if (commit){
-                    FancyToast.makeText(getApplicationContext(),"data saved success..",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
-                }else {
-                    FancyToast.makeText(getApplicationContext(),"data saved fail",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
-                }
-            }
-        });
-
-        //load data button action
-        btnGetData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPreferences prf = getSharedPreferences("details", MODE_PRIVATE);
-                String name = prf.getString("name", "no name");
-                int age = prf.getInt("age", 0);
-
-                FancyToast.makeText(getApplicationContext(),"Your name : "+name+"\nYour age : "+age,FancyToast.LENGTH_LONG,FancyToast.INFO,true).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                String country = String.valueOf(parent.getItemAtPosition(position));
+                FancyToast.makeText(getApplicationContext(),"Selected country: "+country,FancyToast.LENGTH_LONG,FancyToast.INFO,true).show();
             }
         });
     }
