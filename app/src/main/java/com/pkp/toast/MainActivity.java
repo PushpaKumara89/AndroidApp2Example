d50e32836b95147f9a531c453b040e5db76c6db8
kpushpakumara89 <kpushpakumarajd@gmail.com>
2022-12-07 19:53:21 +0530

    Complex ListView /* -Steps 1 * 01) Create string resources * 02) Create single item row design * 03) Create a class and extend ArrayAdapter<String> * 04) Create a constructor and override method
    -Steps
    * 01) Override getView method
    * 02) Inflate the single row design
    * 03) Get reference to the item in single row and return the view
    * 04) Create ListView in activity and reference to that ListView
    * 05) Create object of Adapter
    * 06) Set adapter to the listView
    */

--- app/src/main/java/com/pkp/toast/MainActivity.java
+++ app/src/main/java/com/pkp/toast/MainActivity.java
@@ -1,37 +1,99 @@
 package com.pkp.toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/*
-* Steps to create ListView Static Icon
-* 01) Create Data Source
-* 02) Create the separate XML file to represent single data row (Text View and Image View)
-* 03) Create the ArrayAdapter with passing 1.Context, 2.layout 4. Data Source
-* 04) Put ListView in layout which you would like to put ListView
-* 05) Get reference to layout which you like to populate ListView with ArrayAdapter
-* 06) Finally set the adapter to listView object
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+/*Complex ListView
+-Steps 1
+* 01) Create string resources
+* 02) Create single item row design
+* 03) Create a class and extend ArrayAdapter<String>
+* 04) Create a constructor and override method
+
+-Steps
+* 01) Override getView method
+* 02) Inflate the single row design
+* 03) Get reference to the item in single row and return the view
+* 04) Create ListView in activity and reference to that ListView
+* 05) Create object of Adapter
+* 06) Set adapter to the listView
 */
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
+
+    String title[] = {"Elon Mack", "Sahan Pasindu", "John Done", "Alexa Redolf", "Thomas Mishel",
+            "John Bosco", "Lary Roy", "Code Camp SL"};
+
+    String description[] = {"071 0014445", "+881 151815", "6884 4545454", "+871 5485118", "+554 7871565",
+            "214 7525666", "354 81164848", "458 45256323"};
+
+
+    int image[] = {
+            R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f,
+            R.drawable.g, R.drawable.i };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String week[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        listView = findViewById(R.id.listView);
+        CustomAdapter customAdapter = new CustomAdapter(this, title, description, image);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this,
-                R.layout.single_row,
-                R.id.textView, week);
+        listView.setAdapter(customAdapter);
 
-        listView = findViewById(R.id.list_view);
-        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = String.valueOf(adapterView.getItemAtPosition(i));
+                FancyToast.makeText(getApplicationContext(), "This is : "+name, FancyToast.LENGTH_LONG,FancyToast.INFO, true).show();
+            }
+        });
+    }
+}
 
+class CustomAdapter extends ArrayAdapter<String>{
+    Context context;
+    int[] images;
+    String[] title;
+    String[] des;
+    public CustomAdapter(Context context, String[] title, String[] des, int[] images) {
+        super(context, R.layout.single_row, R.id.title, title);
+        this.context = context;
+        this.images = images;
+        this.title=title;
+        this.des = des;
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.single_row, parent, false);
+        ImageView imageView = row.findViewById(R.id.imageView);
+        TextView title = row.findViewById(R.id.title);
+        TextView description = row.findViewById(R.id.description);
+
+        imageView.setImageResource(images[position]);
+        title.setText(this.title[position]);
+        description.setText(des[position]);
+
+        return row;
+    }
 }
